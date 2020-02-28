aabeb8c76a5bc761cd7c5c6f7b5dc98c658a4ed1
Amber Mohler <ambermohler@iMac.local>
2020-02-28 11:53:12 -0500

    got bubbles rendering to the screen

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import axios from "axios";
 
 const initialColor = {
@@ -10,6 +10,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+
 
   const editColor = color => {
     setEditing(true);
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = props => {
+  const [credentials, setCredentials] = useState({ username: '', password: ''})
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+    .post('/api/login', credentials)
+    .then(res => {
+      //console.log(res)
+      window.localStorage.setItem('token', res.data.payload)
+      props.history.push('/bubbles')
+    })
+    .catch(err => console.log('error in Login', err))
+  }
+
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-      <p>test</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username <br />
+          <input 
+          type='text'
+          name='username'
+          value={credentials.username}
+          onChange={handleChange}
+          />
+        </label>
+
+        <label>
+          Password <br />
+          <input 
+          type='text'
+          name='password'
+          value={credentials.password}
+          onChange={handleChange}
+          />
+        </label>
+        <button>Log In</button>
+      </form>
     </>
   );
 };
